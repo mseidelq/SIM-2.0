ce2c74406d05f287c6aaecbe120a2c8e4d0b6f3c
mseidelq <45979946+mseidelq@users.noreply.github.com>
2019-01-15 15:32:49 -0500

    Ya revisa en BD al usuario

--- pages/jquery/login.js
+++ pages/jquery/login.js
@@ -1,19 +1,24 @@
 $(document).ready(function(){
+
   var datos_usuario;
+
   $("#ingresar").on("click",function () {
     var usr = $("#input_usuario").val();
     var con = $("#input_contrasena").val();
+
     $.ajax({
   	  type: 'GET',
   	  url: "pages/sql/login.php",
   	  data: {'usr':usr, 'contrasena':con},
   	  success: function(data){
-  				var data2 = JSON.parse(data);
-  				datos_usuario = data2;
-  				},
+  				datos_usuario = JSON.parse(data);
+          //alert(data);
+  		},
 
   	  async:false
   	});
-    alert(datos_usuario);
+    if(datos_usuario.usuario) alert(datos_usuario.usuario);
+    else alert(datos_usuario);
   });
+  
 });
--- pages/sql/login.php
+++ pages/sql/login.php
@@ -1,13 +1,37 @@
 <?php
 $servername = "localhost";
 $username = "usuariosim";
-$password = "administrarsim";
+$password = "";
 $dbname = "sim";
 
-	// FUNCION PARA CREAR TIPO DE HABITACION
+	// FUNCION PARA BUSCAR EL USUARIO Y TRAER LA INFORMACION
 	if(isset($_GET["usr"])){
-    $usr = $_GET["usr"]."-".$_GET['contrasena'];
-    echo json_encode($usr);
-    //echo $usr;
+    $usr = $_GET["usr"];
+    $con = $_GET['contrasena'];
+    // CONEXION Y COMANDO
+    $conexion = mysqli_connect($servername,$username,$password, $dbname);
+    $comando = "CALL p_s_consultar_usuarios('$usr','$con')";
+    //REVISA SI LA CONEXION FUE EXITOSA
+    if (!$conexion) {
+			die("Connection failed: " . mysqli_connect_error());
+		}
+    // RESULTADO DEL COMANDO
+		$resultado = mysqli_query($conexion, $comando);
+    $datos = array();
+
+    if($resultado)
+    {
+		  while($row = mysqli_fetch_assoc($resultado))
+			   $datos = $row;
+
+      if(!$datos)
+        $datos = "Error: El usuario no existe o la contrasena esta errada";
+      echo json_encode($datos);
+    }
+    else{
+      $datos = "error";
+      echo json_encode($datos);
+    }
+
   }
 ?>
